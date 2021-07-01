e5fece0e0b5ef9c0144c39d5ca712a6058763387
Tico-hue <lautarotico@gmail.com>
2021-07-01 19:29:13 -0300

    fixing error

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ var index = require("../app/index");
 describe("index - Test", function () {
   describe("Testing the log", function () {
     it("Testing the sum operation", function () {
-      expect(index.a()).to.equal("hol mundo");
+      expect(index.a()).to.equal("hola mundo");
     });
   });
 });
