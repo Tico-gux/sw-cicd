cf0685c42b42757b7fba9f47cf0954eb82311894
Tico-hue <lautarotico@gmail.com>
2021-07-01 11:12:15 -0300

    fixing port

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3020;
 const { a } = require("./index");
 app.get("/", (req, res) => {
   res.send(a());
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
